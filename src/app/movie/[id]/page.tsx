b3be54c51feb198ef39df75fa645fe7b51a94e6c
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 14:56:32 +0300

    feat:implementing /movie/[id] route and page for displaying Movie Datail which will be rendering a MovieDetail Component

---
+++ src/app/movie/[id]/page.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+
+const MoviePage = ({ params }: any) => {
+  return (
+    <>
+      <div className="hero-content text-center m-auto">
+        <div className="max-w-md"></div>
+      </div>
+    </>
+  );
+};
+
+export default MoviePage;
