8123dbc80c300773ed79f9db9a1bafa27ce8b785
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 15:46:19 +0300

    feat: wrapping all child node with Context Porvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import { MovieProvider } from "@/context/MovieContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="flex min-h-screen flex-col items-center p-3">
-          {children}
+          <MovieProvider>{children}</MovieProvider>
         </main>
       </body>
     </html>
