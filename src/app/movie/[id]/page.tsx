394c880d9419a06436705cece11e17e317bab207
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 15:57:00 +0300

    refactor: Refactor MoviePage component for movie details display by utilize context and MovieDetail Component

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,11 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
+import Loading from "@/components/Loading";
+import MovieDetail from "@/components/MovieDetail";
+import { useMovieContext } from "@/context/MovieContext";
+import { useEffect } from "react";
 
 const MoviePage = ({ params }: any) => {
+  const { state, fetchMovieById } = useMovieContext();
+  const { selectedMovie, loading, error } = state;
+
+  useEffect(() => {
+    if (params.id) {
+      fetchMovieById(params.id as string);
+    }
+  }, [params.id]);
+
+  console.log(selectedMovie);
   return (
     <>
       <div className="hero-content text-center m-auto">
-        <div className="max-w-md"></div>
+        <div className="max-w-md">
+          {loading && <Loading />}
+          {error && <p>{error}</p>}
+        </div>
       </div>
+      {selectedMovie && <MovieDetail movie={selectedMovie} />}
     </>
   );
 };
