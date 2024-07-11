9b84111b4e4ae9c708ef3766fbc95fcac481030d
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 16:40:59 +0300

    refactor: change body font from Inter to Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "../styles/globals.css";
 import { MovieProvider } from "@/context/MovieContext";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: "300",
+});
 
 export const metadata: Metadata = {
   title: "Movie Explorer",
