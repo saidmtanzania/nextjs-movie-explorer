e7aece63b1e1697c3e6ae6264b86a87e2cfefcf5
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 15:51:18 +0300

    fix: Configure Next.js for cross-origin and image loading allowed site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "m.media-amazon.com"],
+  },
+  crossOrigin: "anonymous",
+};
 
 export default nextConfig;
