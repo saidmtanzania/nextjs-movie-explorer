921b921dd9c82a91908af73643c4d3fcc8a55cc8
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 14:30:03 +0300

    feat: implementing Hero page as a land page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Movie Explorer</h1>
+          <p className="pt-1">Welcome🤗</p>
+        </div>
+      </div>
+    </>
+  );
 }
