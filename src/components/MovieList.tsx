8f81a48dbc51b47440a58030fbd5cc1709e7d876
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 16:46:27 +0300

    fix: fixing gap from searchbar and movie List segement

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -8,7 +8,7 @@ const MovieList: React.FC = () => {
   const { state } = useMovieContext();
   const { randomMovies, loading, error } = state;
   return (
-    <div className="mx-auto max-w-screen-lg px-3 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-8 md:grid-cols-3 xl:grid-cols-4">
+    <div className="mx-auto max-w-screen-lg px-3 py-8 sm:px-6 sm:py-12 lg:px-10 lg:py-10 md:grid-cols-3 xl:grid-cols-4">
       {loading && <Loading />}
       {error && <p>{error}</p>}
       <div className="mt-0 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-5">
