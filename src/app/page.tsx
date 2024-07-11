f94dae6f2216170622ef3cf55dcb040470f07c36
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 16:30:55 +0300

    fix: fixing searchbar text to align center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,12 @@ import SearchBar from "@/components/SearchBar";
 export default function Home() {
   return (
     <>
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Movie Explorer</h1>
-          <p className="pt-1">Welcome🤗</p>
-        </div>
+      <div className="max-w-md text-center">
+        <h1 className="text-5xl font-bold">Movie Explorer</h1>
+        <p className="pt-1">Welcome🤗</p>
         <SearchBar />
       </div>
+
       <MovieList />
     </>
   );
