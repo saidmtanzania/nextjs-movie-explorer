ae8e52e8f7964870c13412ddbe805fea31435003
saidmtanzania <saidmtanzania@gmail.com>
2024-07-11 15:37:08 +0300

    implementing and defining the initial state of the movie reducer

--- src/context/movieReducer.ts
+++ src/context/movieReducer.ts
@@ -5,3 +5,11 @@ export interface MovieState {
   loading: boolean; // Flag to indicate if data is being loaded
   error: string | null; // Error message if any
 }
+
+// Define the initial state of the movie reducer
+export const initialState: MovieState = {
+  randomMovies: [], // Initialize random movies array as empty
+  selectedMovie: null, // Initialize selected movie as null
+  loading: false, // Set loading flag to false
+  error: null, // Set error message to null
+};
