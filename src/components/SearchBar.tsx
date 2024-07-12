ce6b1afaffe4d5d10083bc438fc3ce1ba3b9bba6
saidmtanzania <saidmtanzania@gmail.com>
2024-07-12 07:44:58 +0300

    feat: implementing refresh button for regenerating random movies

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,12 +2,13 @@
 import { useMovieContext } from "@/context/MovieContext";
 import React, { useState } from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
+import { SlRefresh } from "react-icons/sl";
 
 interface SearchBarProps {}
 
 const SearchBar: React.FC<SearchBarProps> = () => {
   const [query, setQuery] = useState("");
-  const { searchMovies } = useMovieContext();
+  const { searchMovies, fetchRandomMovies } = useMovieContext();
 
   const handleSearch = () => {
     if (query.trim() !== "") {
@@ -25,25 +26,39 @@ const SearchBar: React.FC<SearchBarProps> = () => {
     setQuery(e.target.value);
   };
 
+  const handleRefresh = () => {
+    fetchRandomMovies();
+  };
+
   return (
     <div className="relative mt-4">
-      <input
-        type="text"
-        value={query}
-        onChange={handleChange}
-        onKeyDown={handleKeyPress}
-        placeholder="Search by keyword or name..."
-        className="w-full rounded-md px-2 border-gray-200 py-2.5 pe-10 outline-none shadow-sm sm:text-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white"
-      />
+      <>
+        <input
+          type="text"
+          value={query}
+          onChange={handleChange}
+          onKeyDown={handleKeyPress}
+          placeholder="Search by keyword or name.."
+          className="w-[250px] rounded-md px-2 border-gray-200 py-2.5 pe-10 outline-none shadow-sm text-sm sm:text-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white"
+        />
 
+        <span className="absolute inset-y-0 end-10 grid w-10 place-content-center">
+          <button
+            type="button"
+            onClick={handleSearch}
+            className="text-gray-600 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+          >
+            <BiSearchAlt2 />
+          </button>
+        </span>
+      </>
       <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
-        <button
-          type="button"
-          onClick={handleSearch}
-          className="text-gray-600 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
-        >
-          <BiSearchAlt2 />
-        </button>
+        <div className="tooltip" data-tip="refresh">
+          <SlRefresh
+            onClick={handleRefresh}
+            className="text-gray-600 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer"
+          />
+        </div>
       </span>
     </div>
   );
